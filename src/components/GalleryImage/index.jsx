aea211c7385ca57472c8d7ea1be03c89867564ee
treedledee <tcmeyer7@gmail.com>
2017-10-29 16:32:48 -0700

    second commit

--- src/components/GalleryImage/index.jsx
+++ src/components/GalleryImage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 function GalleryImage({ imageUrl, title, description }) {
-  <article>
-    {title}
-    {description}
-    <img url={imageUrl}/>
-  </article>
+  return (
+    <article>
+      {title}
+      {description}
+      <img alt="cute animal" url={imageUrl} />
+    </article>
+  );
 }
 
 export default GalleryImage;
---
+++ src/containers/App/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;

38a57f959c1a2acd885de764a38aff4f0c34a040
treedledee <tcmeyer7@gmail.com>
2018-01-31 21:12:03 -0800

    add utils folder and update styles

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,11 +1,11 @@
-const API_ROOT = Object.freeze({
+export const API_ROOT = Object.freeze({
   ROOT: 'api.imgur.com',
 });
 
-const API_PATHS = Object.freeze({
+export const API_PATHS = Object.freeze({
   SUBREDDIT_GALLERY: '/3/gallery/r/aww/top/day/1',
 });
 
-const CLIENT_ID = Object.freeze({
+export const CLIENT_ID = Object.freeze({
   ID: 'c7fd9e46b4c809c',
 });
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Theresa&apos;s boilerplate</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { items: [] };
+  }
+
+  componentDidMount() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Client-ID c7fd9e46b4c809c',
+      },
+    };
+    fetch('https://api.imgur.com/3/gallery/r/aww/hot/day/1', options)
+      .then(response => response.json())
+      .then((json) => {
+        console.log('request succeeded with JSON response', json);
+        this.setState({ items: json.data });
+      })
+      .catch((error) => {
+        console.log('request failed', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Theresa&apos;s boilerplate</h1>
+        </header>
+        <p className="app-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <div>Items:</div>
+          { this.state.items.map(item =>
+            (<div>
+              <div>{item.title}</div>
+              <img className="app-image" alt={item.id} src={item.link} />)
+             </div>))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
